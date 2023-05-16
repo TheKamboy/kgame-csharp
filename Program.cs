f4130043d2d6b48da6ae491611f8755350d39f10
TheKamboy <yaluigi10@gmail.com>
2023-05-15 19:40:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Mindmagma.Curses;
+using System;
+
+namespace kgame_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Screen = NCurses.InitScreen();
+            NCurses.NoDelay(Screen, true);
+            NCurses.NoEcho();
+        }
+    }
+}
